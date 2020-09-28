08b520d2cf93e854caa0eee5ea508ebcd3f593a9
MadaZZ <sparshranaazz@gmail.com>
2020-09-28 21:05:24 +0530

    Added font family: Open Sans Condense

--- src/pages/homepage/hompage.component.jsx
+++ src/pages/homepage/hompage.component.jsx
@@ -4,7 +4,6 @@ import DirectoryMenu from '../../components/directory-menu/directory-menu.compon
 
 const HomePage = () => (
     <div className='homepage'>
-        <h1>Welcome to my Homepage</h1>
         <DirectoryMenu />
     </div>
 );
