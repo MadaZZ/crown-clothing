1c43cdc38a6276cc39ad3c1882debdbffd0685b4
MadaZZ <sparshranaazz@gmail.com>
2020-09-29 22:03:56 +0530

    Added component: Collection-preview

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/hompage.component';
+
 import { Switch, Route } from 'react-router-dom'
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Helloooo!</h1>
-    </div>
-  )
-}
+import HomePage from './pages/homepage/hompage.component'
+import ShopPage from './pages/shop-page/shop-page.component'
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop/hats' component={HatsPage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
