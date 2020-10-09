ded1c0c06ef5235d10feee9500a31ec16d0d2e79
MadaZZ <sparshranaazz@gmail.com>
2020-10-09 23:22:22 +0530

    Added firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB1kEZgxTOtcvxhhJ7lq4-3MjuuqhY0ARs",
+    authDomain: "crown-clothing-db-8e8e5.firebaseapp.com",
+    databaseURL: "https://crown-clothing-db-8e8e5.firebaseio.com",
+    projectId: "crown-clothing-db-8e8e5",
+    storageBucket: "crown-clothing-db-8e8e5.appspot.com",
+    messagingSenderId: "1091932027935",
+    appId: "1:1091932027935:web:02f120858f339838b4a92a",
+    measurementId: "G-21CLF6F1FD"
+};
