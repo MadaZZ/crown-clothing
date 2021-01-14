166eea0ca6bb9910d8a0736c0096b2c4562b694c
MadaZZ <sparshranaazz@gmail.com>
2021-01-14 22:52:21 +0530

    Added Cart-items to Cart-Dropdown
    Now we can see the number of items added to the Cart.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.style.scss';
 import AppStrings from "../../general.strings";
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
-            <div className='cart-items' />
+            <div className='cart-items'>
+                {
+                    cartItems.map(cartItem => <CartItem id={cartItem.id} item={cartItem} />)
+                }
+            </div>
             <CustomButton> {AppStrings.GO_TO_CHECKOUT} </CustomButton>
         </div>
     )
 }
 
-export default CartDropdown
+const mapStateToProps = state => ({
+    cartItems: state.cart.cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown)
