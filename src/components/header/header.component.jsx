2c58e19c6b78d6171fb58a661be3eb8fa5ec342a
MadaZZ <sparshranaazz@gmail.com>
2021-01-16 17:22:24 +0530

    Added user selector, updated header component to use selector

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg'
-import './header.style.scss'
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import './header.style.scss';
 
-import { fireAuth } from '../../firebase/firebase.utils'
+import { fireAuth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selector';
+import { selectCurrentUser } from '../../redux/user/user.selector';
 
 const Header = ({ currentUser, cartHidden }) => {
     return (
@@ -28,18 +31,18 @@ const Header = ({ currentUser, cartHidden }) => {
                         :
                         <Link className='option' to='/signin'>SIGN IN</Link>
                 }
-                <CartIcon/>
+                <CartIcon />
             </div>
             {
-              cartHidden ? null : <CartDropdown />
+                cartHidden ? null : <CartDropdown />
             }
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser,
-    cartHidden: state.cart.hidden
+const mapStateToProps = createStructuredSelector({ // This function passes the top level state to all the selectors
+    currentUser: selectCurrentUser,
+    cartHidden: selectCartHidden
 })
 
 
