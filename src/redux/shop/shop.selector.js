4789358338b800d4c0df2d9f0e61e8c2aa7a6707
MadaZZ <sparshranaazz@gmail.com>
2021-01-24 23:27:05 +0530

    Added in-page routing in shop page to load different collections.

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,21 @@
 import { createSelector } from 'reselect';
 
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+}
+
 const shopSelector = state => state.shop;
 
 export const selectShopCollections = createSelector(
     [shopSelector],
     shop => shop.collections
+)
+
+export const selectCollection = collectionURLParam => createSelector(
+    [selectShopCollections],
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionURLParam])
 )
