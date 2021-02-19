1a54a40c153ca44b740cb34941ddf9edfa3ceb84
MadaZZ <sparshranaazz@gmail.com>
2021-02-19 11:41:07 +0530

    Added page links to collection-preview titles

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-
+import { withRouter } from 'react-router-dom';
 import './collection-preview.style.scss'
 
 import CollectionItem from '../collection-item/collection-item.component'
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, ...props }) => {
+    const { routeName, history, match } = props;
     return (
         <div className='collection-preview'>
-            <h1 className='title'>
+            <h1 className='title' onClick={() => history.push(`${match.url}/${routeName}`)}>
                 {title}
             </h1>
             <div className='preview'>
-                {items.filter((item, index) => index < 4 ).map((item) =>
+                {items.filter((item, index) => index < 4).map((item) =>
                     (
-                        <CollectionItem key={item.id} item={item}/>
+                        <CollectionItem key={item.id} item={item} />
                     ))}
             </div>
         </div>
     )
 }
 
-export default CollectionPreview
+export default withRouter(CollectionPreview);
